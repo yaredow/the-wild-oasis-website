4704ebf070070f66d8a874adf9a1ceda412e87b1
yaredow <yaredyilma11@gmail.com>
2024-05-26 18:29:28 +0300

    finish building the account and the reservation pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
-import image1 from '@/public/about-1.jpg';
+import image1 from '@/../public/about-1.jpg';
+import image2 from '@/../public/about-2.jpg';
+import Link from 'next/link';
 
 export const metadata = {
   title: 'About',
@@ -46,7 +48,7 @@ export default function Page() {
 
       <div className='relative aspect-square col-span-2'>
         <Image
-          src='/about-2.jpg'
+          src={image2}
           fill
           className='object-cover'
           alt='Family that manages The Wild Oasis'
@@ -75,12 +77,12 @@ export default function Page() {
           </p>
 
           <div>
-            <a
+            <Link
               href='/cabins'
               className='inline-block mt-4 bg-accent-500 px-8 py-5 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all'
             >
               Explore our luxury cabins
-            </a>
+            </Link>
           </div>
         </div>
       </div>
--- src/app/account/reservations/page.tsx
+++ src/app/account/reservations/page.tsx
@@ -1,1 +1,29 @@
-export default function Page() {}
+import ReservationCard from '@/components/ReservationCard';
+
+export default function Page() {
+  // CHANGE
+  const bookings: any = [];
+
+  return (
+    <div>
+      <h2 className='font-semibold text-2xl text-accent-400 mb-7'>
+        Your reservations
+      </h2>
+
+      {bookings.length === 0 ? (
+        <p className='text-lg'>
+          You have no reservations yet. Check out our{' '}
+          <a className='underline text-accent-500' href='/cabins'>
+            luxury cabins &rarr;
+          </a>
+        </p>
+      ) : (
+        <ul className='space-y-6'>
+          {bookings.map((booking: any) => (
+            <ReservationCard booking={booking} key={booking.id} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
