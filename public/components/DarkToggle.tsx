0fb0a6e316d32d7945d0a33374720e39a90f0787
yaredow <yaredyilma11@gmail.com>
2024-05-26 10:13:32 +0300

    change the shadcn default colors

--- public/components/DarkToggle.tsx
+++ public/components/DarkToggle.tsx
@@ -1,9 +1,7 @@
 'use client';
 
 import * as React from 'react';
-import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
-
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
@@ -11,6 +9,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { Moon, Sun } from 'lucide-react';
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
@@ -18,10 +17,9 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant='outline' size='icon'>
-          <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-          <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-          <span className='sr-only'>Toggle theme</span>
+        <Button variant='outline' className=' rounded-full' size='icon'>
+          <Sun className='h-[1rem] w-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+          <Moon className='absolute h-[1rem] w-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align='end'>
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,3 @@
-import { ModeToggle } from '../../../public/components/DarkToggle';
-
 export default function Navigation() {
-  return (
-    <div>
-      <ModeToggle />
-    </div>
-  );
+  return <div>home</div>;
 }
